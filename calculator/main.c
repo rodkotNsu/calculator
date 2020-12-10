6e7e55d970bb5df8fbfa437f0eaa34ed0605ec69
Родион Котов <rodtdt@yandex.ru>
2020-12-10 18:18:59 +0700

    it`s all

--- calculator/main.c
+++ calculator/main.c
@@ -4,8 +4,9 @@
 #include<malloc.h>
 #include<string.h>
 #include<math.h>
-#define MAX_LEN_EXP 100 
-#define ERROR_OUTPUT -9999
+#define MAX_LEN_EXP 1100
+#define ERROR_SYNTAX_ERROR -9999
+#define ERROR_DIVISION_BY_ZERO -9998
 
 enum SYNTAX_TYPE
 {
@@ -19,8 +20,17 @@ struct SYMBOL_DATA
 	enum SYNTAX_TYPE type;
 };
 typedef struct SYMBOL_DATA SYMBOL_DATA;
+
 SYMBOL_DATA** free_array_symbol(SYMBOL_DATA** arr);
-SYMBOL_DATA** parsing_str(char* str) {
+SYMBOL_DATA** parsing_str_to_array_symbol(char* str);
+SYMBOL_DATA** build_rpn(SYMBOL_DATA** arr);
+
+double parsing_str_to_float(char* str);
+double calculate_by_rpn(SYMBOL_DATA** arr);
+double calculate_by_string(char* string);
+
+
+SYMBOL_DATA** parsing_str_to_array_symbol(char* str) {
 	unsigned int i = -1;
 	unsigned int count = 0;
 	unsigned int check_num = 0;
@@ -32,14 +42,14 @@ SYMBOL_DATA** parsing_str(char* str) {
 	SYMBOL_DATA* new_object = NULL;
 
 	char alf_operation[] = "\n *-/+()";
-	char alf_operand[] = ".0123456789";
+	char alf_operand[] = "0123456789";
 
 	do {
 		i++;
-		if (strchr(alf_operation,str[i])!=NULL){
+		if (strchr(alf_operation, str[i]) != NULL) {
 			if (check_num > 0) {
 				//Exception: Float haven`t float part 
-				if (buffer[check_num-1] == '.') {
+				if (buffer[check_num - 1] == '.') {
 					flag_error = 1;
 					break;
 				}
@@ -54,7 +64,7 @@ SYMBOL_DATA** parsing_str(char* str) {
 			if (str[i] != ' ') {
 				new_object = malloc(sizeof(SYMBOL_DATA));
 
-				buffer = (char*)malloc(sizeof(str) * 10);
+				buffer = (char*)malloc(sizeof(str) * 20);
 				buffer[0] = str[i];
 				buffer[1] = '\0';
 				if (str[i] == '\n')
@@ -69,53 +79,51 @@ SYMBOL_DATA** parsing_str(char* str) {
 			}
 		}
 		else
-		if (strchr(alf_operand, str[i]) != NULL) {
-			if (check_num == 0) {
-				//Exception: Float haven`t integer part 
-				if (str[i] == '.') {
-					flag_error = 1;
-					break;
+			if (strchr(alf_operand, str[i]) != NULL) {
+				if (check_num == 0) {
+					//Exception: Float haven`t integer part 
+					if (str[i] == '.') {
+						flag_error = 1;
+						break;
+					}
+					buffer = malloc(sizeof(char) * 20);
+
+					buffer[0] = str[i];
+					buffer[1] = '\0';
+
+					check_num = 1;
 				}
-				buffer = malloc(sizeof(char) * 10);
+				else {
+					//Exception: Float have more then 1 points 
+					if (str[i] == '.' && buffer != NULL && strchr(buffer, alf_operand[0]) != NULL) {
+						flag_error = 1;
+						break;
+					}
+					buffer[check_num] = str[i];
+					buffer[check_num + 1] = '\0';
 
-				buffer[0] = str[i];
-				buffer[1] = '\0';
-
-				check_num = 1;
-			}
-			else {
-				//Exception: Float have more then 1 points 
-				if (str[i]=='.' && buffer!=NULL && strchr(buffer, alf_operand[0]) != NULL) {
-					flag_error = 1;
-					break;
+					check_num++;
 				}
-				buffer[check_num] = str[i];
-				buffer[check_num + 1] = '\0';
-
-				check_num++;
 			}
-		}
-			   else
-		{
-			//Exception: String have foreign symbol
-			flag_error = 1;
-			break; 
-		}
-	} while (str[i] != '\n' && flag_error==0);
+			else
+			{
+				//Exception: String have foreign symbol
+				flag_error = 1;
+				break;
+			}
+	} while (str[i] != '\n' && flag_error == 0);
 	// exception handling 
 	if (flag_error == 1) {
-		printf("ERROR");
+
 		new_object = malloc(sizeof(SYMBOL_DATA));
 		new_object->type = END;
 		new_object->data = NULL;
 		if (buffer != NULL)
 			free(buffer);
 		arr_exp[count] = new_object;
-	    free_array_symbol(arr_exp);
+		free_array_symbol(arr_exp);
 		return NULL;
 	}
-	 
-
 	return arr_exp;
 }
 SYMBOL_DATA** free_array_symbol(SYMBOL_DATA** arr) {
@@ -132,14 +140,15 @@ SYMBOL_DATA** free_array_symbol(SYMBOL_DATA** arr) {
 
 		}
 		free(arr);
-		arr = NULL;
-		return arr;
+		 
+		return NULL;
 	}
 }
-SYMBOL_DATA** rpn(SYMBOL_DATA** arr) {
+SYMBOL_DATA** build_rpn(SYMBOL_DATA** arr) {
 	int i = 0;
 	int situation_result = 0;
 	int situation_stack = -1;
+	int flag_operand = 0;
 	SYMBOL_DATA** result = malloc(sizeof(SYMBOL_DATA*) * MAX_LEN_EXP);
 	SYMBOL_DATA** stack = malloc(sizeof(SYMBOL_DATA*) * MAX_LEN_EXP);
 
@@ -148,40 +157,41 @@ SYMBOL_DATA** rpn(SYMBOL_DATA** arr) {
 		switch (arr[i]->type)
 		{
 		case  OPERAND:
-			{
-				result[situation_result++] = arr[i];
-			}
-			break;
+		{
+			result[situation_result++] = arr[i];
+			flag_operand = 1;
+		}
+		break;
 		case  OPERATION:
 		{
 			switch ((arr[i]->data)[0])
 			{
-			
+
 			case '(': {
 				stack[++situation_stack] = arr[i];
+				flag_operand = 0;
 			}break;
-			
+
 			case ')':
 			{
 				int flag = 0;
-				while (situation_stack >= 0)
+
+				while (situation_stack >= 0 && stack[situation_stack]->data[0] != '(')
 				{
-					if (stack[situation_stack]->data[0] == '(')
+					if (stack[situation_stack]->data[0] != '(')
 					{
+						result[situation_result++] = stack[situation_stack];
 						flag = 1;
-						break;
 					}
-					result[situation_result++] = stack[situation_stack];
 					situation_stack--;
 				}
-				if (flag == 1) {
-					situation_stack--;
-				}
-				else {
+				if (situation_stack == -1 || (flag == 0 && flag_operand == 0)) {
 					free(stack);
 					free(result);
 					return NULL;
 				}
+				situation_stack--;
+
 			}
 			break;
 
@@ -204,8 +214,8 @@ SYMBOL_DATA** rpn(SYMBOL_DATA** arr) {
 			case '-': {
 				while (situation_stack >= 0 && stack[situation_stack]->data[0] != '(') {
 					result[situation_result++] = stack[situation_stack];
-					situation_stack--;	
-					}
+					situation_stack--;
+				}
 				stack[++situation_stack] = arr[i];
 			}break;
 			}
@@ -230,42 +240,31 @@ SYMBOL_DATA** rpn(SYMBOL_DATA** arr) {
 		i++;
 	} while (1);
 }
-void print_arr(SYMBOL_DATA** arr) {
-	
-	if (arr != NULL) {
-		int i = 0;
-		while ((*arr[i]).type!=END)
-		{
-			printf("%s ", arr[i]->data);
-			i++;
-		}
-	}
-}
+
 double parsing_str_to_float(char* str) {
 	double result = 0;
 	double pow_10 = 1;
 	char* point = strchr(str, '.');
 	if (point != NULL)
-		pow_10 = pow(10, (double)(point - &str[0]-1));
-	else 
+		pow_10 = pow(10, (double)(point - &str[0] - 1));
+	else
 		pow_10 = pow(10, (double)(strlen(str) - 1));
 	int i = 0;
 	while (str[i] != '\0') {
 		if (str[i] != '.')
 		{
 			result += (str[i] - '0') * pow_10;
-			pow_10 /=10.0;
+			pow_10 /= 10.0;
 		}
 		i++;
 	}
 	return result;
 }
-double postfix_to_result(SYMBOL_DATA** arr) {
-	double* stack = malloc(sizeof(double) * 100);
+double calculate_by_rpn(SYMBOL_DATA** arr) {
+	double stack[MAX_LEN_EXP];
 	int situation_stack = -1;
-	int flag_error = 0;
 	int i = 0;
-	while(arr[i]->type != END){
+	while (arr[i]->type != END) {
 		if (arr[i]->type == OPERAND)
 		{
 			situation_stack++;
@@ -274,8 +273,7 @@ double postfix_to_result(SYMBOL_DATA** arr) {
 		else if (arr[i]->type == OPERATION) {
 			if (situation_stack < 1)
 			{
-				flag_error = 1;
-				break;
+				return ERROR_SYNTAX_ERROR;
 			}
 			else {
 				switch (arr[i]->data[0])
@@ -284,9 +282,8 @@ double postfix_to_result(SYMBOL_DATA** arr) {
 					stack[situation_stack - 1] *= stack[situation_stack];
 					break;
 				case '/':
-					if (stack[situation_stack - 1] == 0) {
-						flag_error = 1;
-						break;
+					if (stack[situation_stack] == 0) {
+						return ERROR_DIVISION_BY_ZERO;
 					}
 					else {
 						stack[situation_stack - 1] /= stack[situation_stack];
@@ -301,42 +298,49 @@ double postfix_to_result(SYMBOL_DATA** arr) {
 				}
 				situation_stack--;
 			}
-	    }
+		}
 		i++;
 	}
-	if (flag_error == 0 && situation_stack == 0) {
-	 
-			double result = stack[situation_stack];
-			free(stack);
-			return  result;
-		}
-	free(stack);
-	return ERROR_OUTPUT;
+	if (situation_stack == 0) {
+		return  stack[situation_stack];
+	}
+
+	return ERROR_SYNTAX_ERROR;
 
 
 }
-int main() {
-	char* str_expression = (char*)malloc(sizeof(char) * MAX_LEN_EXP);
-	if (fgets(str_expression, MAX_LEN_EXP, stdin)) {
-	}
-	SYMBOL_DATA** symbol_arr = parsing_str(str_expression);
-	/* TODO:
-	Релизовать input error 
-	*/
+double calculate_by_string(char* string) {
+	SYMBOL_DATA** symbol_arr = parsing_str_to_array_symbol(string);
+	SYMBOL_DATA** symbol_arr_rpn = NULL;
+	double result = ERROR_SYNTAX_ERROR;
 	if (symbol_arr != NULL) {
-		SYMBOL_DATA** symbol_arr_rpn = rpn(symbol_arr);
+		symbol_arr_rpn = build_rpn(symbol_arr);
 		if (symbol_arr_rpn != NULL) {
-			 
-			double result = postfix_to_result(symbol_arr_rpn);
+			result = calculate_by_rpn(symbol_arr_rpn);
 		}
 	}
-	 
+	free(symbol_arr_rpn);
 	free_array_symbol(symbol_arr);
- 
-	free(str_expression);
+	return result;
+}
 
-	_CrtDumpMemoryLeaks();
+int main() {
+	char* str_expression = (char*)malloc(sizeof(char) * MAX_LEN_EXP);
+	double result = 0;
+	if (fgets(str_expression, MAX_LEN_EXP, stdin)) {
 
-
+		result = calculate_by_string(str_expression);
+		if (result == ERROR_SYNTAX_ERROR) {
+			printf("syntax error");
+		}
+		else if (result == ERROR_DIVISION_BY_ZERO) {
+			printf("division by zero");
+		}
+		else
+		{
+			printf("%d", (int)result);
+		}
+		free(str_expression);
+	}
 	return 0;
 }
